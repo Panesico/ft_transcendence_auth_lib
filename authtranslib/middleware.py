47cf70b911497a15375e929fa5a3195f5277b855
Panesico <pranmc7676@protonmail.com>
2024-10-13 18:58:01 +0200

    updated middleware login for guest control

--- authtranslib/middleware.py
+++ authtranslib/middleware.py
@@ -72,12 +72,11 @@ class JWTAuthenticationMiddleware(MiddlewareMixin):
 from functools import wraps
 from django.http import JsonResponse
 
-
 def login_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if not request.user:
-            return JsonResponse({'error': 'Authentication credentials were not provided.'}, status=401)
+        # If user is not authenticated, respond with an error
+        if isinstance(request.user, GuestUser):
+            return JsonResponse({'error': 'Authentication required.'}, status=401)
         return view_func(request, *args, **kwargs)
-
     return _wrapped_view
